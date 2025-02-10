99d11d38e16d79548fba4e64fe3534d4c94543d3
fraigneau <fraigneau86@yahoo.fr>
2025-02-10 22:51:57 +0100

    Setup PayMyBuddy with Gradle.

---
+++ src/test/java/fr/paymybuddy/PaymybuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.paymybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
