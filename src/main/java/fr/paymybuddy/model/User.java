c437200d783dd530cf8625bc3e53a3b4ab35521c
fraigneau <fraigneau86@yahoo.fr>
2025-02-24 12:03:38 +0100

    feat(model): Optimize entity relationships and fix mappings
    - Removed unnecessary ManyToMany relation in `UserConnection`
    - Properly mapped OneToMany relationships in `User` for transactions
    - Ensured correct use of `@MapsId` in `UserConnection`
    - Cleaned up redundant attributes in all entity models
    - Improved database integrity and Hibernate compatibility

--- src/main/java/fr/paymybuddy/model/User.java
+++ src/main/java/fr/paymybuddy/model/User.java
@@ -1,10 +1,18 @@
 package fr.paymybuddy.model;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,4 +37,14 @@ public class User {
 
     @Column(name = "password", nullable = false, length = 100)
     private String password;
+
+    @ManyToMany
+    @JoinTable(name = "user_connections", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "connection_id"))
+    private Set<User> friends;
+
+    @OneToMany(mappedBy = "sender", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Transaction> sentTransactions;
+
+    @OneToMany(mappedBy = "receiver", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Transaction> receivedTransactions;
 }
